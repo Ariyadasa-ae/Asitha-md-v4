bd77b8f5a612359eeebb0cc73ce8ce30d29e49ac
Ariyadasa-ae <ariyadasaudakara@gmail.com>
2025-07-07 07:18:06 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? ‘𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=0EZ2BQJK#VDaNEgdp27nYLCvyyrMMWI0X4jmZabzCqxFcwNdgEzc': process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? ‘𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=dVBUVSBJ#RC7Z6mxI2AZNIhf1uMP3tyauEl-W3oYN-l_LGibX_FI': process.env.SESSION_ID,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SESSION_NAME: process.env.PORT === undefined ? "asitha" : process.env.SESSION_NAME,
 POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgresql://postgres:@Asitha2005b@db.waiqbrnuxkjebghzhovz.supabase.co:5432/postgres' : process.env.POSTGRESQL_URL,
